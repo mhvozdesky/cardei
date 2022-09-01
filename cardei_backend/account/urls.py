a3b673018f7ecb0a337164c1497738ee54cde0b7
mhvozdesky <m.hvozdesky@gmail.com>
2022-09-01 14:36:04 +0300

    Added names to urls

--- cardei_backend/account/urls.py
+++ cardei_backend/account/urls.py
@@ -3,10 +3,10 @@ from account import views
 
 
 urlpatterns = [
-    path('account/reg/', views.RegistrationCardeiUserView.as_view()),
-    path('account/login/', views.LoginCardeiUserView.as_view()),
-    path('account/logout/', views.LogoutCardeiUserView.as_view()),
+    path('account/reg/', views.RegistrationCardeiUserView.as_view(), name='url_reg'),
+    path('account/login/', views.LoginCardeiUserView.as_view(), name='url_login'),
+    path('account/logout/', views.LogoutCardeiUserView.as_view(), name='url_logout'),
     path('profile/', views.CardeiUserProfile.as_view({
         'get': 'retrieve',  # 'put': 'update', 'patch': 'partial_update'
-    })),
+    }), name='url_profile'),
 ]
