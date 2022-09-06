25588fb431c038928c302e701c43deff19ceb9ab
mhvozdesky <m.hvozdesky@gmail.com>
2022-09-06 17:28:55 +0300

    Changed title field in tags to CharField

--- cardei_backend/users_items/models.py
+++ cardei_backend/users_items/models.py
@@ -16,7 +16,7 @@ class Category(models.Model):
 
 
 class Tag(models.Model):
-    title = models.TextField()
+    title = models.CharField(max_length=50, null=False, blank=False)
 
     def __str__(self):
         return self.title
