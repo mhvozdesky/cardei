f3fe6fc01621fc4e57acf9f3bc3896d791d576db
mhvozdesky <m.hvozdesky@gmail.com>
2022-09-07 16:17:14 +0300

    You can add tags when creating an Item

--- cardei_backend/users_items/serializers.py
+++ cardei_backend/users_items/serializers.py
@@ -3,6 +3,14 @@ from rest_framework import serializers
 from users_items import models
 
 
+class TagSerializer(serializers.ModelSerializer):
+    title = serializers.CharField(max_length=50)
+
+    class Meta:
+        model = models.Tag
+        fields = ['title']
+
+
 class DynamicFieldsModelSerializer(serializers.ModelSerializer):
     def __init__(self, *args, **kwargs):
         fields = kwargs.pop('fields', None)
@@ -16,6 +24,40 @@ class DynamicFieldsModelSerializer(serializers.ModelSerializer):
 
 
 class UsersItemsSerializer(DynamicFieldsModelSerializer):
+    tag = TagSerializer(many=True)
+    # tag = serializers.CharField(max_length=50)
+
     class Meta:
         model = models.Element
         fields = '__all__'
+
+    def create(self, validated_data):
+        tags = validated_data.pop('tag', None)
+
+        instance_super = super(UsersItemsSerializer, self).create(validated_data)
+
+        if tags:
+            tag_list = []
+            for tag in tags:
+                tag_inst = self.process_tags(tag['title'], instance_super)
+                tag_list.append(tag_inst)
+
+            instance_super.tag.set(tag_list)
+            instance_super.save()
+
+        return instance_super
+
+    def process_tags(self, tag, instance_super):
+        tag_exists = models.Tag.objects.filter(title=tag).exists()
+        if tag_exists:
+            return models.Tag.objects.filter(title=tag).first()
+
+        return models.Tag.objects.create(title=tag, user=instance_super.user)
+
+        # if not tag_exists:
+        #     models.Tag.objects.create(title=tag, user=instance_super.user)
+
+
+
+    # def validate(self, attrs):
+    #     print
