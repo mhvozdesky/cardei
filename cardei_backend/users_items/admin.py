9f823daefc9ed212623b9454bc4531a54a1c3360
mhvozdesky <m.hvozdesky@gmail.com>
2022-09-07 16:15:56 +0300

    In the admin in the Element, tags are now visible

--- cardei_backend/users_items/admin.py
+++ cardei_backend/users_items/admin.py
@@ -2,8 +2,14 @@ from django.contrib import admin
 from users_items import models
 
 
+class TagInLineElement(admin.TabularInline):
+    model = models.Element.tag.through
+
+
 class ElementAdmin(admin.ModelAdmin):
     readonly_fields = ['date_creation', 'date_update']
+
+    inlines = (TagInLineElement,)
 
 
 admin.site.register(models.Element, ElementAdmin)
