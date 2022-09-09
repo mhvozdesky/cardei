5d4d1e2540ec3fc3966f4a45459138f5d6715f9f
mhvozdesky <m.hvozdesky@gmail.com>
2022-09-09 10:49:00 +0300

    Implemented element deletion

--- cardei_backend/users_items/urls.py
+++ cardei_backend/users_items/urls.py
@@ -6,10 +6,17 @@ urlpatterns = [
     path('items/', views.ItemsViewSet.as_view({
         'get': 'items_list', 'post': 'create_item'
     }), name='url_items'),
-    path('items/<int:pk>/', views.ItemsViewSet.as_view(
-        {'patch': 'partial_update', 'get': 'items_detail'}),
+    path(
+        'items/<int:pk>/',
+        views.ItemsViewSet.as_view(
+            {
+                'patch': 'partial_update',
+                'get': 'items_detail',
+                'delete': 'destroy'
+            }
+        ),
         name='url_items_detail'
-         ),
+    ),
     path('taglist/', views.TagListView.as_view(), name='url_taglist'),
     path('categorylist/', views.CategoryListView.as_view(), name='url_categorylist')
 ]
