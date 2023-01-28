2de478d7fc8ebbfdbaeb0dce94b6b9d60f27b165
mhvozdesky <m.hvozdesky@gmail.com>
2023-01-28 13:31:54 +0200

    Added button Add

--- cardei_frontend/src/components/MiddleDash.vue
+++ cardei_frontend/src/components/MiddleDash.vue
@@ -1,5 +1,10 @@
 <template>
     <div class="content-MiddleDash">
+        <div class="button-area">
+            <div class="btn-set">
+                <div class="btn add-btn"></div>
+            </div>
+        </div>
         <ul class="list-MiddleDash">
             <li class="item-MiddleDash">Реєстрація на filmix</li>
             <li class="item-MiddleDash">Нові замітки</li>
@@ -44,4 +49,28 @@
     .list-MiddleDash li:nth-child(even):hover {
         background-color: #d0d1e5;
     }
+
+    .content-MiddleDash .button-area {
+        height: 35px;
+    }
+
+    .content-MiddleDash .btn {
+        width: 20px;
+        height: 20px;
+        background-image: url("@/assets/images/add.svg") !important;
+        background-color: transparent !important;
+        background-position: center;
+        background-repeat: no-repeat;
+        padding: 0;
+    }
+
+    .content-MiddleDash .btn:hover {
+        background-color: #bbb6b6 !important; 
+        border-radius: 50%;
+        cursor: pointer;
+    }
+
+    .content-MiddleDash .btn:active {
+        background-color: #7f848d !important; 
+    } /* при нажатии */
 </style>
