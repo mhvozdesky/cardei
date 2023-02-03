10031cd7d48143bbf83078dd51b9966f1ff7bfa3
mhvozdesky <m.hvozdesky@gmail.com>
2023-02-03 12:26:55 +0200

    Added a list of category fields to the category output

---
+++ cardei_backend/constants/constants.py
@@ -0,0 +1,52 @@
+items_fields = {
+    'Логін': [
+        'id',
+        'title',
+        'login',
+        'password',
+        'site',
+        'notes',
+        'tag',
+        'date_creation',
+        'date_update',
+        'category',
+        'user'
+    ],
+    'Пароль': [
+        'id',
+        'title',
+        'password',
+        'notes',
+        'tag',
+        'date_creation',
+        'date_update',
+        'category',
+        'user'
+    ],
+    'Замітка': [
+        'id',
+        'title',
+        'text',
+        'tag',
+        'date_creation',
+        'date_update',
+        'category',
+        'user'
+    ],
+    'Банківська карта': [
+        'id',
+        'title',
+        'owner_name',
+        'card_number',
+        'year',
+        'month',
+        'cvv',
+        'pin_code',
+        'notes',
+        'tag',
+        'date_creation',
+        'date_update',
+        'category',
+        'user'
+    ]
+}
