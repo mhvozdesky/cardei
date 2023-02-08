a9324dad9cbbdb3a13c4a762c88cae0353f098ca
mhvozdesky <m.hvozdesky@gmail.com>
2023-02-08 17:54:25 +0200

    update STATICFILES_DIRS

--- cardei_backend/cardei_backend/settings.py
+++ cardei_backend/cardei_backend/settings.py
@@ -131,7 +131,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+
+if DEBUG:
+    STATICFILES_DIRS = [STATIC_DIR]
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = 'media/'
 
