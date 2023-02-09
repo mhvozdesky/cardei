a163ef33f5cf76fd8e1906730ce422fdd81c251f
mhvozdesky <m.hvozdesky@gmail.com>
2023-02-09 11:15:12 +0200

    The project is launched via nginx

--- cardei_backend/entrypoint.sh
+++ cardei_backend/entrypoint.sh
@@ -12,7 +12,7 @@ then
 fi
 
 python manage.py migrate --noinput
-# python manage.py collectstatic --noinput
+python manage.py collectstatic --noinput
 # python manage.py runserver 0.0.0.0:8000
 
 exec "$@"
