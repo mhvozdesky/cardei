dcb3aeac240bd0296d49297f20f57e0c3dab2a34
mhvozdesky <m.hvozdesky@gmail.com>
2025-01-25 23:56:03 +0200

    added 404 status

--- cardei_frontend/src/views/PageNotFound.vue
+++ cardei_frontend/src/views/PageNotFound.vue
@@ -4,7 +4,14 @@
 
 <script>
     export default {
-        name: "PageNotFound"
+        name: "PageNotFound",
+        mounted() {
+        // Set status code 404
+        document.title = "404 - Page Not Found";
+        if (window.history.state) {
+            window.history.replaceState({ ...window.history.state, status: 404 }, '');
+        }
+    }
     }
 </script>
 
